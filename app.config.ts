e64c54502ea1522d38a271cf3e461ff481264aa6
William Chong <me@williamchong.cloud>
2026-07-02 16:22:36 +0800

    🔖 Bump version to 1.2.6

--- app.config.ts
+++ app.config.ts
@@ -59,7 +59,7 @@ const config: ExpoConfig = {
   name: '3ook.com',
   owner: 'likerland',
   slug: '3ook-com-app',
-  version: '1.2.5',
+  version: '1.2.6',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'com.3ook',
