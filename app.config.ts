4d35be40cd9d707aa3bda7f7a13f46da959f077d
William Chong <me@williamchong.cloud>
2026-07-23 19:09:18 +0800

    🔖 Bump version to 1.2.8

--- app.config.ts
+++ app.config.ts
@@ -59,7 +59,7 @@ const config: ExpoConfig = {
   name: '3ook.com 圖書館',
   owner: 'likerland',
   slug: '3ook-com-app',
-  version: '1.2.7',
+  version: '1.2.8',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'com.3ook',
